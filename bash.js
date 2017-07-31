94cadebe4abcf3eff7710d5f36cbfb014418fb39
derrickbeining <derrick.beining@gmail.com>
2017-07-31 11:52:36 -0400

    echo command done

--- bash.js
+++ bash.js
@@ -1,10 +1,11 @@
 const commands = require('./commands.js');
-
 // Output a prompt
 process.stdout.write('prompt > ');
 
 // The stdin 'data' event fires after a user types in a line
 process.stdin.on('data', function (data) {
-  var cmd = data.toString().trim();
-  commands[cmd]();
+  data = data.toString().split(' ');
+  var cmd = data[0].toString().trim();
+  const args = data.slice(1);
+  commands[cmd](args);
 });
--- commands.js
+++ commands.js
@@ -9,13 +9,27 @@ module.exports = {
    out.write('\nprompt > ')
   },
 
+  echo: function(args) {
+    const argsv = args.map(function(el) {
+      if (el[0] === "$") {
+        const envVar = el.slice(1).toUpperCase();
+        console.log('process.env: ', process.env['PATH'])
+        return process.env[envVar];
+      } else {
+        return el;
+      }
+    });
+    out.write(argsv.join(' '));
+    out.write('\nprompt >');
+  },
+
   ls: function() {
     fs.readdir('.', function(err, files) {
       if (err) throw console.error('Error: check yo self');
       files.forEach(function(file) {
         out.write(file.toString() + "\n");
       });
-      out.write('prompt >');
+      out.write('\nprompt >');
     });
   },
 
