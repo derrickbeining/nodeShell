5e207f9327806da5fb0e0ab938021eeabf08da72
derrickbeining <derrick.beining@gmail.com>
2017-07-31 21:14:40 -0400

    implemented piping i/o  and grep

--- bash.js
+++ bash.js
@@ -1,11 +1,44 @@
 const commands = require('./commands.js');
-// Output a prompt
-process.stdout.write('prompt > ');
+const stdout = process.stdout;
+const stdin = process.stdin;
 
-// The stdin 'data' event fires after a user types in a line
-process.stdin.on('data', function (data) {
-  data = data.toString().split(' ');
-  var cmd = data[0].toString().trim();
-  const args = data.slice(1);
-  commands[cmd](args);
+// const exec = function(prevOutput, cmd, args, handleDone, cmdList) {
+
+// }
+
+const handleDone = function(output, pipedCmdList) {
+  if (pipedCmdList.length) {
+    let cmdTokens = pipedCmdList.shift().split(' ');
+      let cmd = cmdTokens[0];
+      let args = cmdTokens.length > 2 ? cmdTokens.slice(1) : cmdTokens[1];
+      if (commands[cmd]) {
+        commands[cmd](output, args, handleDone, pipedCmdList);
+      } else {
+        handleDone('Error: No such command. Try again.')
+      }
+  } else {
+    stdout.write(output);
+    stdout.write('\nprompt >');
+  }
+}
+
+stdout.write('prompt > ');
+
+stdin.on('data', function (data) {
+  const cmdList = data.toString().trim().split(/\s*\|\s*/g);
+  let cmdTokens = cmdList.shift().split(' ');
+  let cmd = cmdTokens[0];
+  let args = cmdTokens.length > 2 ? cmdTokens.slice(1) : cmdTokens[1];
+  if (commands[cmd]) {
+    commands[cmd](null, args, handleDone, cmdList);
+  } else {
+    handleDone('Error: No such command. Try again.')
+  }
+  // var cmd = data[0].toString().trim();
+  // const args = data.length > 2 ? data.slice(1) : data[1];
+  // if (commands[cmd]) {
+  //   commands[cmd](args, done);
+  // } else {
+  //   done('Error: No such command. Try again.')
+  // }
 });
--- commands.js
+++ commands.js
@@ -1,84 +1,105 @@
 
 const fs = require('fs');
 const request = require('request');
-const out = process.stdout;
+// const out = process.stdout;
 
 module.exports = {
-  cat: function(file){
-    fs.readFile(file[0].trim(), function(err, data){
-      if(err) throw err
-      else {out.write(data.toString());
-        out.write("\nprompt > ")
+  cat: function(stdin, file, done, pipedCmdList){
+    fs.readFile(file, function(err, data){
+      if (err) { throw console.error(err) }
+      else {
+        done(data.toString(), pipedCmdList);
       }
     });
   },
 
-  curl: function(url){
-    request(url[0].trim(), function (error, response, body) {
-      if(error) throw error;
-      out.write(body);
-      out.write("\nprompt > ");
-});
-
+  curl: function(stdin, url, done, pipedCmdList){
+    request(url, function (err, response, body) {
+      if (err) { throw console.error(err) }
+      else { done(body, pipedCmdList) }
+    });
   },
 
-  date: function() {
-   out.write(new Date().toString());
-   out.write('\nprompt > ')
+  date: function(stdin, file, done, pipedCmdList) {
+    done(new Date().toString(), pipedCmdList);
   },
 
-  echo: function(args) {
+  echo: function(stdin, args, done, pipedCmdList) {
+    Array.isArray(args) ? args : args = args.split();
     const argsv = args.map(function(el) {
-      if (el[0] === "$") {
-        const envVar = el.slice(1).toUpperCase();
-        console.log('process.env: ', process.env['PATH'])
-        return process.env[envVar];
+      if (el[0] === '$') {
+        return process.env[el.slice(1).toUpperCase()];
       } else {
         return el;
       }
     });
-    out.write(argsv.join(' '));
-    out.write('\nprompt >');
+    done(argsv.join(' '), pipedCmdList);
   },
 
-  head: function(file){
-    fs.readFile(file[0].trim(), function(err, data){
-      if(err) throw err
-      else {
-        const doc = data.toString().split("\n").slice(0,5).join("\n");
-        out.write(doc);
-        out.write("\nprompt > ")
-      }
-    });
-  },
-
-  ls: function() {
-    fs.readdir('.', function(err, files) {
-      if (err) throw console.error('Error: check yo self');
-      files.forEach(function(file) {
-        out.write(file.toString() + "\n");
+  grep: function(stdin, args, done, pipedCmdList) {
+    const matchStr = Array.isArray(args) ? args[0] : args;
+    const fileName = args[1] ? args[1] : null;
+    function grepper(text) {
+      return text.split('\n')
+              .filter( el => el.toLowerCase().search(matchStr) >= 0 )
+              .join('\n');
+    }
+    if (stdin) {
+      done(grepper(stdin), pipedCmdList);
+    } else {
+      fs.readFile(fileName, function(err, data) {
+        if (err) {
+          done(err);
+        } else {
+          done(grepper(data.toString()), pipedCmdList);
+        }
       });
-      out.write('\nprompt >');
-    });
+    }
   },
 
-  pwd: function () {
-    out.write(process.cwd());
-    out.write('\nprompt > ');
+  head: function(stdin, file, done, pipedCmdList){
+    if (stdin) {
+      done(stdin.split('\n').slice(0, 2).join('\n'), pipedCmdList);
+    } else {
+      fs.readFile(file, function(err, data){
+        if (err) {
+          throw console.error(err)
+        } else {
+          done(data.toString().split('\n').slice(0, 2).join('\n'), pipedCmdList);
+        }
+      });
+    }
   },
 
-  tail: function(file){
-        fs.readFile(file[0].trim(), function(err, data){
-      if(err) throw err
+  ls: function(stdin, path, done, pipedCmdList) {
+    path = path || '.';
+    fs.readdir(path, function(err, files) {
+      if (err) { throw console.error(err); }
       else {
-        const doc = data.toString().split("\n").slice(-6).join("\n");
-        out.write(doc);
-        out.write("\nprompt > ")
+        const output = files.reduce(function(accum, file) {
+          return accum + file.toString() + '\n';
+        }, '');
+        done(output, pipedCmdList);
       }
     });
-
   },
 
+  pwd: function(stdin, file, done, pipedCmdList) {
+    done(process.cwd(), pipedCmdList);
+  },
 
+  tail: function(stdin, file, done, pipedCmdList){
+    if (stdin) {
+      done(stdin.toString().trim().split('\n').slice(-2).join('\n'), pipedCmdList)
+    } else {
+      fs.readFile(file, function(err, data){
+        if (err) { throw console.error(err) }
+        else {
+          done(data.toString().trim().split('\n').slice(-2).join('\n'), pipedCmdList);
+        }
+      });
+    }
+
+  }
 }
 
